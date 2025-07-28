d1c1eb07aeab9fdb73f999b722d3dd42a063424a
zeus9990 <86179151+zeus9990@users.noreply.github.com>
2025-07-28 21:42:35 +0530

    Update config.py
    Added Zencreators in role

--- config.py
+++ config.py
@@ -7,7 +7,8 @@ ROLE_REWARD = {"1316232347816955964": ["zenMiner | Zenrock", 1000],
                "1268395930499944539": ["zenDisciple | Zenrock", 2500],
                "1268396133236084766": ["zenMaster | Zenrock", 10000],
                "1268396272855941224": [".zen | Zenrock", 20000],
-               "1362246524066861267": ["ROCK 1 | Zenrock", 30000]}
+               "1362246524066861267": ["ROCK 1 | Zenrock", 30000],
+               "1289437312136122500": ["zenContent Creators | Zenrock", 5000]}
 RUNNER = [1226891544334700667]
 
 MAIN_COLOR = 0x060f42
