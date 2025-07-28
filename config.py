e530603d6c5354a63b859805d528d5ae71b41e65
zeus9990 <86179151+zeus9990@users.noreply.github.com>
2025-07-28 21:44:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 DB_URL = os.getenv("DB_URL")
 ROLE_REWARD = {"1316232347816955964": ["zenMiner | Zenrock", 1000],
                "1268395930499944539": ["zenDisciple | Zenrock", 2500],
+               "1289437312136122500": ["zenContent Creators | Zenrock", 5000],
                "1268396133236084766": ["zenMaster | Zenrock", 10000],
                "1268396272855941224": [".zen | Zenrock", 20000],
-               "1362246524066861267": ["ROCK 1 | Zenrock", 30000],
-               "1289437312136122500": ["zenContent Creators | Zenrock", 5000]}
+               "1362246524066861267": ["ROCK 1 | Zenrock", 30000]}
 RUNNER = [1226891544334700667]
 
 MAIN_COLOR = 0x060f42
