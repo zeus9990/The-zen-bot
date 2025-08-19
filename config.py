0fd76e2b8282f64d6ac12d80e2dd45a3f436881f
zeus9990 <86179151+zeus9990@users.noreply.github.com>
2025-08-19 22:01:15 +0530

    Update config.py
    Added Rock 2 role for claiming

--- config.py
+++ config.py
@@ -8,9 +8,11 @@ ROLE_REWARD = {"1316232347816955964": ["zenMiner | Zenrock", 1000],
                "1289437312136122500": ["zenContent Creators | Zenrock", 5000],
                "1268396133236084766": ["zenMaster | Zenrock", 10000],
                "1268396272855941224": [".zen | Zenrock", 20000],
-               "1362246524066861267": ["ROCK 1 | Zenrock", 30000]}
+               "1362246524066861267": ["ROCK 1 | Zenrock", 30000],
+               "1362246632661323920": ["ROCK 2 | Zenrock", 30000]}
 RUNNER = [1226891544334700667]
 
 MAIN_COLOR = 0x060f42
 GREEN = 0x66FF00
 RED = 0xFF6666
+
